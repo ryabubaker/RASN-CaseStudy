0f5cfce439aa70330db9771a8c9b1b0a20f6ed01
ryabubaker <63212411+ryabubaker@users.noreply.github.com>
2024-09-25 16:47:03 +0300

    security and other fixes

--- src/main/java/com/example/rasnassesment/security/UserPrincipal.java
+++ src/main/java/com/example/rasnassesment/security/UserPrincipal.java
@@ -63,19 +63,16 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
